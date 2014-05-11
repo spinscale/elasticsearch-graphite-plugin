3f4bcf65dba290d266ce1547114a694fcd8e1626
damienclaveau <damien.claveau@gmail.com>
2014-05-11 08:34:18 +0100

    Implement some unit tests to check the specific case of a non-master node

--- src/test/java/org/elasticsearch/module/graphite/test/GraphitePluginIntegrationTest.java
+++ src/test/java/org/elasticsearch/module/graphite/test/GraphitePluginIntegrationTest.java
@@ -18,6 +18,7 @@ import static org.hamcrest.Matchers.notNullValue;
 
 public class GraphitePluginIntegrationTest {
 
+    public static final String GRAPHITE_SERVER_HOST = "localhost";
     public static final int GRAPHITE_SERVER_PORT = 12345;
 
     private GraphiteMockServer graphiteMockServer;
@@ -25,6 +26,7 @@ public class GraphitePluginIntegrationTest {
     private String clusterName = UUID.randomUUID().toString().replaceAll("-", "");
     private String index = UUID.randomUUID().toString().replaceAll("-", "");
     private String type = UUID.randomUUID().toString().replaceAll("-", "");
+    private String defaultPrefix = "elasticsearch." + clusterName;
     private Node node;
 
     @Before
@@ -43,22 +45,23 @@ public class GraphitePluginIntegrationTest {
 
     @Test
     public void testThatIndexingResultsInMonitoring() throws Exception {
-        node = createNode(clusterName,  GRAPHITE_SERVER_PORT, "1s");
+        node = createNode(clusterName, true, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", null);
         IndexResponse indexResponse = indexElement(node, index, type, "value");
         assertThat(indexResponse.getId(), is(notNullValue()));
 
         Thread.sleep(2000);
 
         ensureValidKeyNames();
-        assertGraphiteMetricIsContained("elasticsearch." + clusterName + ".indexes." + index + ".id.0.indexing._all.indexCount 1");
-        assertGraphiteMetricIsContained("elasticsearch." + clusterName + ".indexes." + index + ".id.0.indexing." + type + ".indexCount 1");
-        assertGraphiteMetricIsContained("elasticsearch." + clusterName + ".node.jvm.threads.peakCount ");
+        assertGraphiteMetricIsContained(defaultPrefix + ".indexes." + index + ".id.0.indexing._all.indexCount 1");
+        assertGraphiteMetricIsContained(defaultPrefix + ".indexes." + index + ".id.0.indexing." + type + ".indexCount 1");
+        assertGraphiteMetricIsContained(defaultPrefix + ".node.jvm.threads.peakCount ");
     }
+
 
     @Test
     public void testThatFieldExclusionWorks() throws Exception {
         String excludeRegex = ".*\\.peakCount";
-        node = createNode(clusterName, GRAPHITE_SERVER_PORT, "1s", null, excludeRegex);
+        node = createNode(clusterName, true, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", null, null, excludeRegex);
 
         IndexResponse indexResponse = indexElement(node, index, type, "value");
         assertThat(indexResponse.getId(), is(notNullValue()));
@@ -66,14 +69,14 @@ public class GraphitePluginIntegrationTest {
         Thread.sleep(2000);
 
         ensureValidKeyNames();
-        assertGraphiteMetricIsNotContained("elasticsearch." + clusterName + ".node.jvm.threads.peakCount ");
+        assertGraphiteMetricIsNotContained(defaultPrefix + ".node.jvm.threads.peakCount ");
     }
 
     @Test
     public void testThatFieldInclusionWinsOverExclusion() throws Exception {
         String excludeRegex = ".*" + clusterName + ".*";
         String includeRegex = ".*\\.peakCount";
-        node = createNode(clusterName, GRAPHITE_SERVER_PORT, "1s", includeRegex, excludeRegex);
+        node = createNode(clusterName, true, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", null, includeRegex, excludeRegex);
 
         IndexResponse indexResponse = indexElement(node, index, type, "value");
         assertThat(indexResponse.getId(), is(notNullValue()));
@@ -81,26 +84,28 @@ public class GraphitePluginIntegrationTest {
         Thread.sleep(2000);
 
         ensureValidKeyNames();
-        assertGraphiteMetricIsNotContained("elasticsearch." + clusterName + ".indexes." + index + ".id.0.indexing._all.indexCount 1");
-        assertGraphiteMetricIsContained("elasticsearch." + clusterName + ".node.jvm.threads.peakCount ");
+        assertGraphiteMetricIsNotContained(defaultPrefix + ".indexes." + index + ".id.0.indexing._all.indexCount 1");
+        assertGraphiteMetricIsContained(defaultPrefix + ".node.jvm.threads.peakCount ");
     }
 
     @Test(expected = ProvisionException.class)
     public void testThatBrokenRegexLeadsToException() throws Exception {
         String excludeRegex = "*.peakCount";
-        createNode(clusterName, GRAPHITE_SERVER_PORT, "1s", null, excludeRegex);
+        createNode(clusterName, true, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", null, null, excludeRegex);
     }
-
 
     @Test
     public void masterFailOverShouldWork() throws Exception {
-        node = createNode(clusterName, GRAPHITE_SERVER_PORT, "1s");
+        // Generate a new cluster not to interfer with the precedents tests
         String clusterName = UUID.randomUUID().toString().replaceAll("-", "");
+        String defaultPrefix = "elasticsearch." + clusterName;
+
+        node = createNode(clusterName, true, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", null);
         IndexResponse indexResponse = indexElement(node, index, type, "value");
         assertThat(indexResponse.getId(), is(notNullValue()));
 
         Node origNode = node;
-        node = createNode(clusterName, GRAPHITE_SERVER_PORT, "1s");
+        node = createNode(clusterName, true, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", null);
         graphiteMockServer.content.clear();
         origNode.stop();
         indexResponse = indexElement(node, index, type, "value");
@@ -108,7 +113,48 @@ public class GraphitePluginIntegrationTest {
 
         // wait for master fail over and writing to graph reporter
         Thread.sleep(2000);
-        assertGraphiteMetricIsContained("elasticsearch." + clusterName + ".indexes." + index + ".id.0.indexing._all.indexCount 1");
+        assertGraphiteMetricIsContained(defaultPrefix + ".indexes." + index + ".id.0.indexing._all.indexCount 1");
+    }
+
+    @Test
+    public void testThatNotMasterNodeIsMonitoredAsWell() throws Exception {
+        // Generate a new cluster not to interfer with the precedents tests
+        String clusterName = UUID.randomUUID().toString().replaceAll("-", "");
+        String defaultPrefix = "elasticsearch." + clusterName;
+
+        // create a master node  without the plugin disabled
+        Node masterNode = createNode(clusterName, true);
+        graphiteMockServer.content.clear();
+        Thread.sleep(2000);
+        // create a non-master node with the plugin enabled, 
+        // and a specific prefix to be sure that the metrics are emitted by that node
+        String specificPrefix = defaultPrefix + ".dataNode";
+        node = createNode(clusterName, false, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", specificPrefix);
+        Thread.sleep(2000);
+
+        assertGraphiteMetricIsContained(specificPrefix + ".node.jvm.uptime ");
+    }
+
+    @Test
+    public void testThatNotMasterNodeIsNotMonitoredWhenNoMasterIsFound() throws Exception {
+        // Generate a new cluster not to interfer with the precedents tests
+        String clusterName = UUID.randomUUID().toString().replaceAll("-", "");
+        String defaultPrefix = "elasticsearch." + clusterName;
+
+        // create a non-master node with the plugin enabled, 
+        // and no metric should be reported until a master is elected
+        String specificPrefix = defaultPrefix + ".dataNode";
+        graphiteMockServer.content.clear();
+        node = createNode(clusterName, false, GRAPHITE_SERVER_HOST, GRAPHITE_SERVER_PORT, "1s", specificPrefix);
+        Thread.sleep(4000);
+        assertGraphiteMetricIsNotContained(specificPrefix + ".node.jvm.uptime ");
+
+        // create a master node to be joigned...
+        graphiteMockServer.content.clear();
+        Node masterNode = createNode(clusterName, true);
+        Thread.sleep(4000);
+        //the dataNode should now start reporting metrics
+        assertGraphiteMetricIsContained(specificPrefix + ".node.jvm.uptime ");
     }
 
     // the stupid hamcrest matchers have compile erros depending whether they run on java6 or java7, so I rolled my own version
--- src/test/java/org/elasticsearch/module/graphite/test/NodeTestHelper.java
+++ src/test/java/org/elasticsearch/module/graphite/test/NodeTestHelper.java
@@ -10,22 +10,34 @@ import java.io.IOException;
 
 public class NodeTestHelper {
 
-    public static Node createNode(String clusterName, int graphitePort, String refreshInterval) throws IOException {
-        return createNode(clusterName, graphitePort, refreshInterval, null, null);
+    public static Node createNode(String clusterName, boolean nodeMaster) throws IOException {
+        return createNode(clusterName, nodeMaster, null, 0, null, null, null, null);
     }
 
-    public static Node createNode(String clusterName, int graphitePort, String refreshInterval, String includeRegex,
+    public static Node createNode(String clusterName, boolean nodeMaster, String graphiteHost, int graphitePort, String refreshInterval, String graphitePrefix) throws IOException {
+        return createNode(clusterName, nodeMaster, graphiteHost, graphitePort, refreshInterval, graphitePrefix, null, null);
+    }
+
+    public static Node createNode(String clusterName, boolean nodeMaster, String graphiteHost, int graphitePort, String refreshInterval, String graphitePrefix, String includeRegex,
                                   String excludeRegex) throws IOException {
         ImmutableSettings.Builder settingsBuilder = ImmutableSettings.settingsBuilder();
 
         settingsBuilder.put("gateway.type", "none");
         settingsBuilder.put("cluster.name", clusterName);
+        settingsBuilder.put("node.master", nodeMaster);
         settingsBuilder.put("index.number_of_shards", 1);
         settingsBuilder.put("index.number_of_replicas", 1);
-
-        settingsBuilder.put("metrics.graphite.host", "localhost");
-        settingsBuilder.put("metrics.graphite.port", graphitePort);
-        settingsBuilder.put("metrics.graphite.every", refreshInterval);
+        settingsBuilder.put("discovery.zen.ping.multicast.enabled", false);
+        settingsBuilder.put("discovery.zen.ping.unicast.hosts",  "localhost:9300,localhost:9301,localhost:9302");
+        
+        if (Strings.hasLength(graphiteHost)) {
+          settingsBuilder.put("metrics.graphite.host", graphiteHost);
+          settingsBuilder.put("metrics.graphite.port", graphitePort);
+          settingsBuilder.put("metrics.graphite.every", refreshInterval);
+            if (Strings.hasLength(graphitePrefix)) {
+                settingsBuilder.put("metrics.graphite.prefix", graphitePrefix);
+            }
+        }
 
         if (Strings.hasLength(includeRegex)) {
             settingsBuilder.put("metrics.graphite.include", includeRegex);
