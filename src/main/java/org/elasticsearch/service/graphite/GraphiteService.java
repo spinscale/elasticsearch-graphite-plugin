4e4c86120182c73910fdb445d336b09e2ccc1cf9
Mikalai Zubok <mikalai.zubok@gmail.com>
2017-01-25 13:15:34 -0500

    Removing node role check as for client nodes it always returns false for all roles

--- src/main/java/org/elasticsearch/service/graphite/GraphiteService.java
+++ src/main/java/org/elasticsearch/service/graphite/GraphiteService.java
@@ -103,7 +103,7 @@ public class GraphiteService extends AbstractLifecycleComponent<GraphiteService>
                 DiscoveryNode node = clusterService.localNode();
                 boolean isClusterStarted = clusterService.lifecycleState().equals(Lifecycle.State.STARTED);
 
-                if (isClusterStarted && node != null && (node.isMasterNode() || node.isDataNode() || node.isClientNode()) ) {
+                if (isClusterStarted && node != null) {
                     NodeIndicesStats nodeIndicesStats = indicesService.stats(false);
                     CommonStatsFlags commonStatsFlags = new CommonStatsFlags().clear();
                     NodeStats nodeStats = nodeService.stats(commonStatsFlags, true, true, true, true, true, true, true, true, true);
